47ba6b6a609435c7f303d964d7b37cd7681704bb
N1tr0N1x <nizar.zitouni@univ-constantine2.dz>
2021-03-17 15:53:46 +0100

    Teacher Microervice

---
+++ src/test/java/com/eler/ms/course/Teacher/Service/TeacherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eler.ms.course.Teacher.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
