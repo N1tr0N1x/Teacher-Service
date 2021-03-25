0514af30e5d4648dc1a57966b7228e50e01b23da
seryas17 <seryas.ratised@gmail.com>
2021-03-25 10:46:11 +0100

    teacher services creational

--- src/main/java/com/eler/ms/course/Teacher/Service/Controllers/TeacherController.java
+++ src/main/java/com/eler/ms/course/Teacher/Service/Controllers/TeacherController.java
@@ -1,11 +1,18 @@
 package com.eler.ms.course.Teacher.Service.Controllers;
 
+import java.util.List;
+
 import com.eler.ms.course.Teacher.Service.Models.TeacherAccount;
 import com.eler.ms.course.Teacher.Service.Services.TeacherService;
+import com.google.gson.Gson;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +25,31 @@ public class TeacherController {
     @RequestMapping("/registerEmptyTeacher/{idTeacher}")
     public TeacherAccount RegisterEmptyTeacher(@PathVariable("idTeacher") int idTeacher){
         return teacherService.CreateEmptyTeacherAccount(idTeacher);
+    }
+
+    @PostMapping("/saveTeacher")
+    @ResponseBody
+    public void SaveTeacher(@RequestBody TeacherAccount teacher) {
+        teacherService.SaveTeacherAccount(teacher);
+    }
+
+    @GetMapping(value = "/teachers")
+    @ResponseBody
+    public String getAllTeachers() {
+        List<TeacherAccount> teachers = teacherService.listAll();
+        String data = new Gson().toJson(teachers);
+        return data;
+    }
+
+    @GetMapping(value = "/delete/{id}")
+    @ResponseBody
+    public void deleteTeacher(@PathVariable("id") int id) {
+        teacherService.delete(id);
+    }
+
+    @GetMapping(value = "/getTeacher/{id}")
+    @ResponseBody
+    public TeacherAccount getModule(@PathVariable("id") int id) {
+        return teacherService.getTeacher(id);
     }
 }
--- src/main/java/com/eler/ms/course/Teacher/Service/Services/TeacherService.java
+++ src/main/java/com/eler/ms/course/Teacher/Service/Services/TeacherService.java
@@ -1,10 +1,13 @@
 package com.eler.ms.course.Teacher.Service.Services;
 
+import java.util.List;
+
 import com.eler.ms.course.Teacher.Service.Models.TeacherAccount;
 import com.eler.ms.course.Teacher.Service.Repositories.TeacherAccountRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class TeacherService {
@@ -22,5 +25,22 @@ public class TeacherService {
         TeacherAccount teacher = new TeacherAccount(idTeacher,email,firstName,lastName);
         teacherAccRepository.save(teacher);
         return teacher;
+    }
+
+    public TeacherAccount SaveTeacherAccount(TeacherAccount teacher) {
+        teacherAccRepository.save(teacher);
+        return teacher;
+    }
+
+    public List<TeacherAccount> listAll() {
+        return teacherAccRepository.findAll();
+    }
+
+    public void delete(int idTeacher) {
+        teacherAccRepository.deleteById(idTeacher);
+    }
+
+    public TeacherAccount getTeacher(int id) {
+        return teacherAccRepository.findById(id).get();
     }
 }
